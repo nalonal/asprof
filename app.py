305608a3e9606095acdc78c0d50a69a79d4f570f
nalonal <rionaldyrizqy@gmail.com>
2023-03-08 12:10:40 +0000

    update

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def tor_requests(url):
 class GetOpenAPI(threading.Thread):
         def __init__(self, id):
                 self.id = id
-                super(GetOpenfrom8ortGetOpenfrom8ortfGetOpenfrom8ortGetOpenfrom8ortfff).__init__()
+                super(GetOpenAPI, self).__init__()
                 self.headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'} 
                 self.cr = Crossref()
 
